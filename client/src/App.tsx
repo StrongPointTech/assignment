24a597164f23079dbcd244e364611ec1ab1a7ee5
dpekus <pekus.dovydas@gmail.com>
2022-02-06 18:01:08 +0000

    Add a basic form to the client
    This needs styling, also does not do any calculations yet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,95 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { EnergyCalculationRequestModel } from './models/energy-calculation-request-model';
+import { Field, Form, Formik } from 'formik';
 
 function App() {
+  const requestModel: EnergyCalculationRequestModel = {
+    mass: 0,
+    massUnit: "kg",
+    velocity: 0,
+    velocityUnit: "m/s",
+    energyUnit: "J",
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <h1 className="header">Kinetic Energy calculator</h1>
+        <div className="form-container">
+          <Formik
+            initialValues={requestModel}
+            onSubmit={(values, actions) => {
+              console.log({ values, actions });
+              alert(JSON.stringify(values, null, 2));
+              actions.setSubmitting(false);
+            }}
+          >
+            <Form>
+              <span className="first-column">Measurement</span>
+              <span className="second-column">Value</span>
+              <span className="third-column">Unit</span>
+
+              <div className="form-group">
+                <label>
+                  <span className="first-column">
+                    <i>energy</i> m =
+                  </span>
+                  <span className="second-column">
+                    <Field type="number" name="mass" placeholder="Mass" />
+                  </span>
+                  <span className="third-column">
+                    <Field as="select" name="massUnit">
+                      <option value="kg">kg</option>
+                      <option value="g">g</option>
+                      <option value="oz">oz</option>
+                      <option value="lb">lb</option>
+                    </Field>
+                  </span>
+                </label>
+              </div>
+              
+              <div className="form-group">
+                <label>
+                  <span className="first-column">
+                    <i>velocity</i> v =
+                  </span>
+                  <span className="second-column">
+                    <Field type="number" name="velocity" placeholder="Velocity" />
+                  </span>
+                  <span className="third-column">
+                    <Field as="select" name="velocityUnit">
+                      <option value="m/s">m/s</option>
+                      <option value="km/h">km/h</option>
+                      <option value="ft/s">ft/s</option>
+                      <option value="mi/h">mi/h</option>
+                    </Field>
+                  </span>
+                </label>
+              </div>
+
+              <div className="form-group">
+                <label>
+                  <span className="first-column">
+                    <i>energy</i> KE =
+                  </span>
+                  <span className="second-column"></span>
+                  <span className="third-column">
+                    <Field as="select" name="energyUnit">
+                      <option value="J">J</option>
+                      <option value="MJ">MJ</option>
+                      <option value="BTU">BTU</option>
+                      <option value="cal">cal</option>
+                    </Field>
+                  </span>
+                </label>
+              </div>
+
+              <button type="submit">Calculate</button>
+            </Form>
+          </Formik>
+        </div>
+      </div>
     </div>
   );
 }
