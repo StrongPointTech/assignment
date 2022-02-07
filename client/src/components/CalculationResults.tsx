7fc7fbcd4a72a2e1e56f0731799f04d0e045efe6
dpekus <pekus.dovydas@gmail.com>
2022-02-07 22:45:55 +0000

    Some styling

--- client/src/components/CalculationResults.tsx
+++ client/src/components/CalculationResults.tsx
@@ -8,34 +8,38 @@ interface Props {
 export default function CalculationResults({results}: Props) {
   return (
     <div className="results-container">
-      <h3>Results</h3>
+      <h1>Results</h1>
       <div className="table-container">
-        <table>
-          <thead>
-            <tr>
-              <th style={{width: 50}}>Masss</th>
-              <th style={{width: 20}}>Mass Unit</th>
-              <th style={{width: 50}}>Velocity</th>
-              <th style={{width: 20}}>Velocity Unit</th>
-              <th style={{width: 50}}>Energy</th>
-              <th style={{width: 20}}>Energy Unit</th>
-              <th style={{width: 500}}>Impact</th>
-            </tr>
-          </thead>
-          <tbody>
-            {results.map((result, index) => (
-              <tr key={index}>
-                <td>{result.mass}</td>
-                <td>{result.massUnit}</td>
-                <td>{result.velocity}</td>
-                <td>{result.velocityUnit}</td>
-                <td>{result.energy}</td>
-                <td>{result.energyUnit}</td>
-                <td>{result.impact}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        {results.length > 0 ? (
+          <>
+            <table>
+              <thead>
+                <tr>
+                  <th style={{width: 50}}>Masss</th>
+                  <th style={{width: 20}}>Mass Unit</th>
+                  <th style={{width: 50}}>Velocity</th>
+                  <th style={{width: 20}}>Velocity Unit</th>
+                  <th style={{width: 50}}>Energy</th>
+                  <th style={{width: 20}}>Energy Unit</th>
+                  <th style={{width: 500}}>Impact</th>
+                </tr>
+              </thead>
+              <tbody>
+                {results.map((result, index) => (
+                  <tr key={index}>
+                    <td>{result.mass}</td>
+                    <td>{result.massUnit}</td>
+                    <td>{result.velocity}</td>
+                    <td>{result.velocityUnit}</td>
+                    <td>{result.energy}</td>
+                    <td>{result.energyUnit}</td>
+                    <td>{result.impact}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </>
+        ) : <span>Results to your calculations will be displayed here</span>}
       </div>
     </div>
   );
