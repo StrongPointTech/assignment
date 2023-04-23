98ca183f7d4c7e672a7d12ce75be83def64a1f78
GintasS <svedgintas@gmail.com>
2023-04-23 20:51:36 +0300

    Domain project

---
+++ src/StrongPointTechAssignment.Domain/KineticEnergyImpactCalculatorHelper.cs
@@ -0,0 +1,32 @@
+﻿using StrongPointTechAssignment.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrongPointTechAssignment.Domain
+{
+    public static class KineticEnergyImpactCalculatorHelper
+    {
+        public static KineticEnergyImpactToEarthLevels CalculateLevel(decimal? energyReleased)
+        {
+            if (energyReleased.HasValue is false)
+            {
+                return KineticEnergyImpactToEarthLevels.None;
+            }
+            else if (energyReleased < 1000)
+            { 
+                return KineticEnergyImpactToEarthLevels.Minimal;
+            }
+            else if (energyReleased > 1000 && energyReleased < 10000)
+            {
+                return KineticEnergyImpactToEarthLevels.Medium;
+            }
+            else
+            {
+                return KineticEnergyImpactToEarthLevels.High;
+            }
+        }
+    }
+}
